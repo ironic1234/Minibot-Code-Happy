09719f95495466cb711a5215f8a1fba337dddac8
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2024-10-12 14:00:35 -0400

    added gimbal task

--- app/src/chassis_task.c
+++ app/src/chassis_task.c
@@ -1,19 +1,17 @@
 #include "chassis_task.h"
 
-#include "robot.h"
 #include "remote.h"
+#include "robot.h"
 
 extern Robot_State_t g_robot_state;
 extern Remote_t g_remote;
 
 float chassis_rad;
 
-void Chassis_Task_Init()
-{
-    // Init chassis hardware
+void Chassis_Task_Init() {
+  // Init chassis hardware
 }
 
-void Chassis_Ctrl_Loop()
-{
-    // Control loop for the chassis
+void Chassis_Ctrl_Loop() {
+  // Control loop for the chassis
 }
--- app/src/gimbal_task.c
+++ app/src/gimbal_task.c
@@ -1,18 +1,41 @@
 #include "gimbal_task.h"
 
-#include "robot.h"
 #include "remote.h"
+#include "robot.h"
 #include "user_math.h"
 
 extern Robot_State_t g_robot_state;
 extern Remote_t g_remote;
 
-void Gimbal_Task_Init()
-{
-    // Init Gimbal Hardware
+DJI_Motor_Handle_t *yawMotor;
+
+void Gimbal_Task_Init() {
+  Motor_Config_t yaw_speed_config = {
+    .can_bus = 1,
+    .speed_controller_id = 1,
+    .offset = 0,
+    .control_mode = POSITION_CONTROL,
+    .motor_reversal = MOTOR_REVERSAL_NORMAL,
+    .velocity_pid = 
+      {
+        .kp = 0.00f;
+        .ki = 0.00f, 
+        .kd = 0.00f, 
+        .output_limit = M2006_MAX_CURRENT,
+      }
+    .angle_pid = 
+      {
+	.kp = 0.00f,
+	.ki = 0.00f,
+	.kd = 0.00f,
+	.output_limit = M2006_MAX_CURRENT,
+	.integral_limit = 0.00f,
+      }
+  };
+
+  yawMotor = DJI_Motor_Init(&yaw_speed_config, M2006);
 }
 
-void Gimbal_Ctrl_Loop()
-{
-    // Control loop for gimbal
+void Gimbal_Ctrl_Loop() {
+  // Control loop for gimbal
 }
